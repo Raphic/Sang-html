f968647eec5f2123dea2264e10403438b2ad751d
Destop Home <tndkhoa1104@gmail.com>
2017-11-07 03:00:03 +0700

    update the new oil

--- theoil-new/js/script.js
+++ theoil-new/js/script.js
@@ -1,5 +1,16 @@
- 
-
+ var swiper = new Swiper('.slider-01 .swiper-container', {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    speed: 800,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
 
 var swiper = new Swiper('.slider-02 .swiper-container', {
     spaceBetween: 40,
@@ -17,3 +28,17 @@ var swiper = new Swiper('.slider-02 .swiper-container', {
         prevEl: '.swiper-button-prev',
     },
 });
+
+var swiper = new Swiper('.slider-03 .swiper-container', {
+    slidesPerView: 4,
+    spaceBetween: 55,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
