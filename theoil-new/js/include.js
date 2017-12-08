d59e1e1ea19c70ff074fd9e48afac7f3bf10ea91
Khoa Nguyen Dang Tran <khoandtran@ilavietnam.com>
2017-12-08 16:32:30 +0700

    Update samples

--- theoil-new/js/include.js
+++ theoil-new/js/include.js
@@ -11,9 +11,11 @@ function isIE() {
 include('js/jquery-2.1.1.js');
 include('js/jquery-ui.min.js');
 include('js/swiper.min.js');
+//include('js/jquery.mobile.custom.min.js');
 //include(domain + '/js/jquery.mCustomScrollbar.js');
 /* Responsive ========================================================*/
 include('js/responsive/priority-nav.js');
 include('js/responsive/classie.js');
 include('js/responsive/modernizr.custom.js');
 
+include('js/compare-img.js');
