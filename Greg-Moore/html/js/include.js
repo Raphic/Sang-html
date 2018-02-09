baf170b0040210d077aaaa7a5f266d44a57d1e06
Khoa Nguyen Dang Tran <khoandtran@ilavietnam.com>
2018-02-09 17:00:44 +0700

    Update Greg Moore

---
+++ Greg-Moore/html/js/include.js
@@ -0,0 +1,17 @@
+function include(scriptUrl) {
+    document.write('<script src="' + scriptUrl + '"></script>');
+}
+
+function isIE() {
+    var myNav = navigator.userAgent.toLowerCase();
+    return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
+};
+
+/* Main js========================================================*/
+include('js/jquery-2.1.1.js');
+include('js/jquery-ui.min.js');
+include('https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.1.0/js/swiper.min.js');
+//include(domain + '/js/jquery.mCustomScrollbar.js');
+
+
+
